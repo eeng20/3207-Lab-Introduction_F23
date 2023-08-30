5c3374e61287a8731fce684f6d6aa91c7b3c65f5
Estelee Eng <tul40997@temple.edu>
2023-08-30 10:30:15 -0400

    attempt 3

--- random.c
+++ random.c
@@ -1,11 +1,12 @@
 char randomLetter;
 
 randomLetter randomchar() {
-	char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
-                          'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+        char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
+                          'h', 'i', 'j', 'k', 'l', 'm', 'n',
                           'o', 'p', 'q', 'r', 's', 't', 'u',
                           'v', 'w', 'x', 'y', 'z' };
-	string randomLetter = "";
-	randomLetter = alphabet[rand() % 26];
-	return randomLetter;
+        string randomLetter = "";
+        randomLetter = alphabet[rand() % 26];
+        return randomLetter;
 }
+
