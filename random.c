7a8b5821b642c84cd568858894d544e602c56e18
eeng20 <143618011+eeng20@users.noreply.github.com>
2023-08-30 10:45:26 -0400

    updated randchar

--- random.c
+++ random.c
@@ -1,11 +1,10 @@
 char randomLetter;
 
-char randomchar() {
+char randchar() {
         char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
                           'h', 'i', 'j', 'k', 'l', 'm', 'n',
                           'o', 'p', 'q', 'r', 's', 't', 'u',
                           'v', 'w', 'x', 'y', 'z' };
-        string randomLetter = "";
         randomLetter = alphabet[rand() % 26];
         return randomLetter;
 }
