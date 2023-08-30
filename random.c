78a96371c013cc4d2d966ee7dfddc71bc3fe130d
Estelee Eng <tul40997@temple.edu>
2023-08-30 10:10:01 -0400

    attempt 2

--- random.c
+++ random.c
@@ -1,6 +1,6 @@
 char randomLetter;
 
-randomLetter randomchar(char chara) {
+randomLetter randomchar() {
 	char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
                           'h', 'i', 'j', 'k', 'l', 'm', 'n', 
                           'o', 'p', 'q', 'r', 's', 't', 'u',
---
+++ random.h
@@ -0,0 +1,2 @@
+char randomLetter;
+randomLetter randomchar();
