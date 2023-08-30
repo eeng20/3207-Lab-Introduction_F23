293222465c30aaec48b875b595672f8935fac52d
eeng20 <143618011+eeng20@users.noreply.github.com>
2023-08-30 10:44:52 -0400

    changed return type

--- random.c
+++ random.c
@@ -1,6 +1,6 @@
 char randomLetter;
 
-randomLetter randomchar() {
+char randomchar() {
         char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
                           'h', 'i', 'j', 'k', 'l', 'm', 'n',
                           'o', 'p', 'q', 'r', 's', 't', 'u',
